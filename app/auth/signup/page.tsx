26a010eea5ade661aee901f53895e08fd87ede6e
amgenious <henrytweneboah956@gmail.com>
2024-06-25 11:48:09 +0000

    added the signup functions

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -11,7 +11,33 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { GiHypersonicBolt } from "react-icons/gi";
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/components/util/firebase"
 const SignupForm =()=> {
+  const router = useRouter();
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+  
+  
+
+  const handlesignin=(e:any) => {
+    e.preventDefault()
+  
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+    router.push('/auth/login');
+  })
+  .catch((error) => {
+   setError(true)
+   setErrorMessage(error.message);
+  });
+  }
   return (
     <div className="flex flex-1 justify-center items-center h-screen">
     <Card className="mx-auto max-w-sm">
@@ -26,7 +52,7 @@ const SignupForm =()=> {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
+        <form className="grid gap-4"  onSubmit={handlesignin}>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
@@ -34,16 +60,18 @@ const SignupForm =()=> {
               type="email"
               placeholder="example@gmail.com"
               required
+              onChange={e=>setEmail(e.target.value)}
             />
           </div>
           <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
+          <Input id="password" type="password" onChange={e=>setPassword(e.target.value)} required />
           </div>
           <Button type="submit" className="w-full">
             Sign up
           </Button>
-        </div>
+          {error && <span>{errorMessage}</span>}
+        </form>
         <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
           <Link href="/auth/login" className="underline">
